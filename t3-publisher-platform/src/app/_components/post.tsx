81f2f9dfcfa37f4a38d517c0cb641a97d2cef13a
ahmadalnajjar01 <ahmadalnajjar609@gmail.com>
2026-02-10 20:16:02 +0300

    fix trpc

--- t3-publisher-platform/src/app/_components/post.tsx
+++ t3-publisher-platform/src/app/_components/post.tsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 import { api } from "~/trpc/react";
 
 export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
+  const [latestPost] = api.posts.getLatest.useSuspenseQuery();
 
   const utils = api.useUtils();
   const [name, setName] = useState("");
-  const createPost = api.post.create.useMutation({
+  const createPost = api.posts.create.useMutation({
     onSuccess: async () => {
       await utils.post.invalidate();
       setName("");
